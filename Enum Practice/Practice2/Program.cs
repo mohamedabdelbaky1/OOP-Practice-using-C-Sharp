6a98abafe9646b74e60e676b7ba64d0de814b0ab
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2025-09-16 19:30:52 +0300

    Add files via upload

---
+++ Enum Practice/Practice2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2-Create an Enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members.
+            //Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season.
+            //Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+
+
+            bool IsParsed;
+            Seasons season;
+            do
+            {
+                Console.WriteLine("Please enter the Season Name : ");                
+                IsParsed = Enum.TryParse(Console.ReadLine().ToUpper(), out season);
+            } while (!IsParsed);
+            switch(season)
+            {
+                case Seasons.AUTUMN :
+                    Console.WriteLine("September to November");
+                    break;
+                case Seasons.SUMMER:
+                    Console.WriteLine("June to August");
+                    break;
+                case Seasons.SPRING:
+                    Console.WriteLine("March to May");
+                    break;
+                case Seasons.WINTER:
+                    Console.WriteLine("December to February");
+                    break;
+            }
+        }
+    }
+}
---
+++ Enum Practice/Practice2/Seasons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    internal enum Seasons : byte
+    {
+        SPRING = 1 ,
+        SUMMER = 2 , 
+        AUTUMN = 3 ,
+        WINTER = 4
+    }
+}
