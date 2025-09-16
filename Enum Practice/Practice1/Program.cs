e17dee713efa4c95b79908582602fb6ad4310631
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2025-09-16 18:48:10 +0300

    Add files via upload

---
+++ Enum Practice/Practice1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace EnumPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - Create an Enum called "WeekDay" with the days of the week(Monday to Sunday)
+            //as its members.Then, write a C# program that prints out all the days of the week using this Enum.
+
+            foreach(WeekDay day in Enum.GetValues(typeof(WeekDay)))
+            {
+                Console.WriteLine(day);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Enum Practice/Practice1/WeekDay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice
+{
+    internal enum WeekDay
+    {
+        Saturday = 0 ,
+        Sunday = 1 , 
+        Monday = 2 ,
+        Tuesday =3 , 
+        Wednesday = 4 ,
+        Thursday = 5 ,
+        Friday = 6 
+    }
+}
